7c6d96e8309666b7f4f24f47d8eb9d3f6a606394
Ryan Rodrigues <90661822+ryanpanta@users.noreply.github.com>
2023-07-29 20:40:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def homepage():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", port=10000)
                            
